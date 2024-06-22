aaa3722360eb4b785de0968144e0fc8259e6d323
GabrielCoelho <gbcoe@outlook.com.br>
2024-06-22 11:36:05 -0300

    Data Type -> Arrays

--- data_types/src/main.rs
+++ data_types/src/main.rs
@@ -1,3 +1,5 @@
+use std::io;
+
 fn main() {
     // Integer types. (Usage of underscore to let the LSP know that this variables won't be used)
     //          Signed types can be negative
@@ -59,4 +61,36 @@ fn main() {
     let _five_hundred = _a_tuple.0; // accessing the first value of _a_tuple (500)
     let _five_point_three = _a_tuple.1; // acessing the second value of _a_tuple (5.3)
     let _one = _a_tuple.2; // accessing the third value of _a_tuple (1)
+
+    // Arrays
+    let _array1 = [1, 2, 3, 4, 5]; // you can either declare a array without the explicit annotation
+    let _array2: [u16; 5] = [5, 4, 3, 2, 1]; // or with the explicit annotation, where first it
+                                             // comes the data type of your array, and second the
+                                             // number of elements it will have.
+    let _array3 = [2; 3]; // Or even have an array of same value for each element -> this _array3 is
+                          // the same as writing `let _array3 = [2,2,2];`
+
+    // Accessing array elements.
+    let _first_element = _array1[0]; // as simple as in c
+
+    // Be careful to not reach an invalid element.
+    println!("Please enter an array index.");
+    let mut index = String::new();
+
+    // standard reading from terminal
+    io::stdin()
+        .read_line(&mut index)
+        .expect("Failed to read line");
+
+    let index: usize = index
+        .trim()
+        .parse()
+        .expect("Index entered was not a number");
+
+    let element = _array1[index];
+
+    println!("The value of the element at index {index} is: {element}");
+    // the code above will panic if you put an index greater than the max_length of the _array1,
+    // which is 4. This code will compile because Rust check at runtime, as the compiler can't
+    // possibly know what value a user will enter when they run the code later.
 }
