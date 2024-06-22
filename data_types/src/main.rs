5cfc38ca252afc98a3a4e00f423d1fd632f556a8
GabrielCoelho <gbcoe@outlook.com.br>
2024-06-22 11:22:33 -0300

    Data Type -> Tuple

--- data_types/src/main.rs
+++ data_types/src/main.rs
@@ -46,4 +46,17 @@ fn main() {
                                // and U+E000 to U+10FFFF
 
     // Tuple Type
+    let _a_tuple: (i32, f64, u8) = (500, 5.3, 1);
+    // once declared, a tuple cannot change its size. In the above example, the tuple has 3
+    // different data types in it. I cannot shrink it to two, or grow it to five. It's only three
+    // for all the execution of the program.
+
+    // Accessing a tupple value
+    // We can assign the value to other variables like below:
+    let (_ax, _ay, _az) = _a_tuple; // where _ax = 500; _ay = 5.3; and _az = 1.
+
+    // Or accessing by the tuple index (starting always in 0)
+    let _five_hundred = _a_tuple.0; // accessing the first value of _a_tuple (500)
+    let _five_point_three = _a_tuple.1; // acessing the second value of _a_tuple (5.3)
+    let _one = _a_tuple.2; // accessing the third value of _a_tuple (1)
 }
