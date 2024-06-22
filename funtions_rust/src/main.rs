e6b7e59c1132f7716742942e19f63753a2bafc22
GabrielCoelho <gbcoe@outlook.com.br>
2024-06-22 11:47:18 -0300

    Functions -> Passing Parameters/Arguments

--- funtions_rust/src/main.rs
+++ funtions_rust/src/main.rs
@@ -1,8 +1,22 @@
 fn main() {
     println!("Hello, world!");
     another_fn();
+
+    fn_args(5);
+    print_labeled_measurement(5, 'h');
 }
 
 fn another_fn() {
-    print!("This is from another function");
+    println!("This is from another function");
+}
+
+// Passing an argument or a parameter, you alwais MUST declare the type of each one of your
+// parameters. In this case, the function below is recieving an integer of 8 bits, and printing it
+// after.
+fn fn_args(x: i8) {
+    println!("The value of x is {x}");
+}
+
+fn print_labeled_measurement(value: i32, unit_label: char) {
+    println!("The measurement is: {value}{unit_label}");
 }
