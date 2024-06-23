a87440046121406f468a3ee0048bec35a8a5ffd3
GabrielCoelho <gbcoe@outlook.com.br>
2024-06-23 13:26:08 -0300

    ownership -> mutable strings

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -5,4 +5,11 @@ fn main() {
         let _s = "hello"; // now that an inside "S" is declared, is now valid this point forward
                           // do stuff with s
     } // this scope is over, and s is no longer valid
+
+    // Learning about the String Type
+    {
+        let mut s = String::from("hello");
+        s.push_str(", world"); // append to the string
+        println!("{s}"); // print "Hello, world"
+    }
 }
