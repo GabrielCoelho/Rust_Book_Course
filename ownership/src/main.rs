cfe4ebae504698c03acfc0efd5c5b043a2bc774f
GabrielCoelho <gbcoe@outlook.com.br>
2024-06-23 13:34:19 -0300

    Ownership -> knowing about borrowing

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -11,5 +11,11 @@ fn main() {
         let mut s = String::from("hello");
         s.push_str(", world"); // append to the string
         println!("{s}"); // print "Hello, world"
+
+        let _s2 = s;
+        // println!("{s}"); <- causes an error, because we already assigned the pointer value of s
+        // to a new variable. This is called "Borrowing" in rust. Once something is borrowed, rust
+        // consider s as no longer valid.
+        println!("{_s2}");
     }
 }
