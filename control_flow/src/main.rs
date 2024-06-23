7f3c7d40a131df85b763f978f2569303a03170bd
GabrielCoelho <gbcoe@outlook.com.br>
2024-06-22 22:47:40 -0300

    Control Flow -> Else If

--- control_flow/src/main.rs
+++ control_flow/src/main.rs
@@ -1,5 +1,5 @@
 fn main() {
-    let number = 3;
+    let number = 6;
 
     // If statement
     // // Note: it doesn'n need any parenthesis to check, only if you want to encapsulate more
@@ -8,5 +8,15 @@ fn main() {
         println!("condition was true");
     } else {
         println!("condition was false");
+    }
+
+    if number % 4 == 0 {
+        println!("number was divisible by 4");
+    } else if number % 3 == 0 {
+        println!("number was divisible by 3");
+    } else if number % 2 == 0 {
+        println!("number was divisible by 2");
+    } else {
+        println!("number was not divisible by 4, 3 or 2");
     }
 }
