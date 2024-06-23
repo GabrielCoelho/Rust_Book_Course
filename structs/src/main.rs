51c581f360f567fff104d22988ce9e9bb94700d4
GabrielCoelho <gbcoe@outlook.com.br>
2024-06-23 17:35:49 -0300

    refactor(tuple): Is easier to do with a tuple type

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -26,16 +26,11 @@ fn main() {
     }
 
     {
-        let width1 = 30;
-        let heigth1 = 50;
-
-        println!(
-            "The area of the rectangle is {} square pixels",
-            area(width1, heigth1)
-        );
+        let rect1 = (30, 50);
+        println!("The area of the rectangle is {} square pixels", area(rect1));
     }
 }
 
-fn area(w: u32, h: u32) -> u32 {
-    w * h
+fn area(dimensions: (u32, u32)) -> u32 {
+    dimensions.0 * dimensions.1
 }
