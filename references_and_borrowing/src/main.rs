192f30885242a270298ee917942d67e2c8e7af16
GabrielCoelho <gbcoe@outlook.com.br>
2024-06-23 14:47:46 -0300

    Borrowing -> References with &

--- references_and_borrowing/src/main.rs
+++ references_and_borrowing/src/main.rs
@@ -8,10 +8,15 @@ fn main() {
     // Passing the address of the variable, we maintain it usable after passing it to a function.
     let len = calculate_length(&s1);
 
+    // The & syntax create a reference that refers to the value of s1 but does not own it.
+    // Because it does not own it, the value it points to will not be dropped when the reference
+    // stops being used.
+
     println!("The length of '{s1}' is {len}.");
 }
 
 // But you need to specify that you're getting a address
 fn calculate_length(s: &String) -> usize {
     s.len()
-}
+} // Here, s goes out of scope. But because it does not have ownership of what
+  // it refers to, it is not dropped.
