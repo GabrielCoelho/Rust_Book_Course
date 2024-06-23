ba927c7b1f29ede23f99281747288478f310cbb8
GabrielCoelho <gbcoe@outlook.com.br>
2024-06-23 13:50:37 -0300

    Ownership -> understanding more about memory freeing

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -23,4 +23,21 @@ fn main() {
         println!("First: {s} / Second {s2}");
         // by using `.clone()` we keep both variables usable.
     }
-}
+
+    let s = String::from("hello"); // new variable s comes into the scope
+
+    takes_ownership(s); // s's value moves to a function...
+                        // ... and so is no longer valid here (more info in the comments of the function)
+    let x = 5; // x comes into the scope
+    makes_copy(x); // x would move into the function, but i32 is Copy so...
+                   // ...x can be used afterwards
+} // Here, x goes out of the scope.
+
+fn takes_ownership(lilstring: String) {
+    println!("{lilstring}");
+} // from this point, rust compiler calls a `drop` and the memory is freed. So, even in the main
+  // function, the String is no longer valid from the point where this function is closed.
+
+fn makes_copy(integerr: i32) {
+    println!("{integerr}");
+} // here integerr goes out of scope. Nothing special happens with x
