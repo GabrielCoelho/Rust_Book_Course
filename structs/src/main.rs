67386aead26c45b8cb887a1c9e66040ac20f5d67
GabrielCoelho <gbcoe@outlook.com.br>
2024-06-23 17:45:25 -0300

    Struct Methods -> more than `&self` parameter

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -7,6 +7,9 @@ impl Rect {
     fn area(&self) -> u32 {
         self.width * self.height
     }
+    fn can_hold(&self, other: &Rect) -> bool {
+        self.width > other.width && self.height > other.height
+    }
 }
 
 fn main() {
@@ -41,9 +44,16 @@ fn main() {
             width: 30,
             height: 50,
         };
+        let rect2 = Rect {
+            width: 50,
+            height: 100,
+        };
         println!(
             "The area of the rectangle is {} square pixels",
             rect1.area()
         );
+
+        println!("Can rect1 hold rect2 ? {}", rect1.can_hold(&rect2));
+        println!("Can rect2 hold rect1 ? {}", rect2.can_hold(&rect1));
     }
 }
