15fd79d352ec1736f7e695ca06154dcb2d4a407a
GabrielCoelho <gbcoe@outlook.com.br>
2024-06-22 22:41:12 -0300

    Control Flow section -> If Statement

---
+++ control_flow/src/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+    let number = 3;
+
+    // If statement
+    // // Note: it doesn'n need any parenthesis to check, only if you want to encapsulate more
+    // logical statements
+    if number < 5 {
+        println!("condition was true");
+    } else {
+        println!("condition was false");
+    }
+}
