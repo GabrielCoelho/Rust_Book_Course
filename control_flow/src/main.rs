66ab0f2644e960e9c9d364fe15a7eaeeee8a8f2b
GabrielCoelho <gbcoe@outlook.com.br>
2024-06-22 22:54:45 -0300

    Control Flow -> Loops

--- control_flow/src/main.rs
+++ control_flow/src/main.rs
@@ -26,4 +26,18 @@ fn main() {
     let number2: u8 = if condition { 5 } else { 6 };
 
     println!("The value of the number is {number2}"); // gonna be 5, because condition is true
+
+    // let number3:u8 = if condition {5} else {"six"}; <- This will cause an error, because is
+    // mismatching the types. number3 is a unsigned integer of 8 bits, which cannot take a string
+    // such as "six" as its value.
+    learning_loops();
+}
+
+// Learning Loops.
+fn learning_loops() {
+    // This is an example of a endless loop, because it doesn't have any condition to exit this
+    // repetition block.
+    loop {
+        println!("again!");
+    }
 }
