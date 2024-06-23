9a130c246824c7aca7d7a0bbdb21b79227873f8f
GabrielCoelho <gbcoe@outlook.com.br>
2024-06-23 17:49:12 -0300

    Struct -> Associated Functions

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -10,6 +10,12 @@ impl Rect {
     fn can_hold(&self, other: &Rect) -> bool {
         self.width > other.width && self.height > other.height
     }
+    fn square(size: u32) -> Self {
+        Self {
+            width: size,
+            height: size,
+        }
+    }
 }
 
 fn main() {
@@ -48,6 +54,7 @@ fn main() {
             width: 50,
             height: 100,
         };
+        let rect3 = Rect::square(20);
         println!(
             "The area of the rectangle is {} square pixels",
             rect1.area()
@@ -55,5 +62,9 @@ fn main() {
 
         println!("Can rect1 hold rect2 ? {}", rect1.can_hold(&rect2));
         println!("Can rect2 hold rect1 ? {}", rect2.can_hold(&rect1));
+        println!(
+            "rect3 is actually a square, because it has width = {} and height={}",
+            rect3.width, rect3.height
+        );
     }
 }
