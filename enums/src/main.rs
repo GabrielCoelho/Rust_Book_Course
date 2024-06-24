b0613c61763ce11578672923c844027652c4bcce
GabrielCoelho <gbcoe@outlook.com.br>
2024-06-23 21:54:15 -0300

    Learning the similarities between enums and structs

--- enums/src/main.rs
+++ enums/src/main.rs
@@ -3,8 +3,17 @@ enum IpAddrKind {
     V6,
 }
 
+struct IpAddress {
+    kind: IpAddrKind,
+    address: String,
+}
+
 fn main() {
     let four = IpAddrKind::V4;
     let six = IpAddrKind::V6;
 
+    let _localhost = IpAddress {
+        kind: IpAddrKind::V4,
+        address: String::from("127.0.0.1"),
+    };
 }
