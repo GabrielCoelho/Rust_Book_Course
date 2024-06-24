0a08f813a2f802c3a245df5e30ae67539968542c
GabrielCoelho <gbcoe@outlook.com.br>
2024-06-23 21:56:07 -0300

    Removing the Struct to keep only the Enum

--- enums/src/main.rs
+++ enums/src/main.rs
@@ -1,19 +1,13 @@
 enum IpAddrKind {
-    V4,
-    V6,
+    V4(String),
+    V6(String),
 }
 
-struct IpAddress {
-    kind: IpAddrKind,
-    address: String,
-}
+//struct IpAddress {
+//  kind: IpAddrKind,
+//address: String,
+//}
 
 fn main() {
-    let four = IpAddrKind::V4;
-    let six = IpAddrKind::V6;
-
-    let _localhost = IpAddress {
-        kind: IpAddrKind::V4,
-        address: String::from("127.0.0.1"),
-    };
+    let _localhost = IpAddrKind::V4(String::from("127.0.0.1"));
 }
