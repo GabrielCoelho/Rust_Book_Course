ce2ba74f668332b5e39cae447e76f89fcd41c288
GabrielCoelho <gbcoe@outlook.com.br>
2024-06-23 22:01:57 -0300

    The Enum "Message"

--- enums/src/main.rs
+++ enums/src/main.rs
@@ -1,13 +1,10 @@
-enum IpAddrKind {
-    V4(String),
-    V6(String),
+enum Message {
+    Quit,                       // Doesn't have  any associated data at all
+    Move { x: i32, y: i32 },    // has named fields, like a struct
+    Write(String),              // tuple
+    ChangeColor(i32, i32, i32), // tuple
 }
 
-//struct IpAddress {
-//  kind: IpAddrKind,
-//address: String,
-//}
-
 fn main() {
-    let _localhost = IpAddrKind::V4(String::from("127.0.0.1"));
+    let m = Message::Write(String::from("This is my message"));
 }
