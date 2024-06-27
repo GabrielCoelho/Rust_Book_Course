0393f1860bc799be4c8f5c55aaf94745621b86c6
GabrielCoelho <gbcoe@outlook.com.br>
2024-06-27 10:17:25 -0300

    Collections -> Vec

---
+++ collections/src/main.rs
@@ -0,0 +1,6 @@
+mod vectors;
+use crate::vectors::vector_collection;
+
+fn main() {
+    vector_collection();
+}
---
+++ collections/src/vectors/mod.rs
@@ -0,0 +1,4 @@
+pub fn vector_collection() {
+    let v: Vec<i32> = Vec::new();
+    let v = vec![1, 2, 3];
+}
