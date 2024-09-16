8e4d6ec228b1eb72eec5923c8c9a602ce739c350
GabrielCoelho <gbcoe@outlook.com.br>
2024-09-16 13:18:48 -0300

    Learning more about Vecs

--- collections/src/vectors/mod.rs
+++ collections/src/vectors/mod.rs
@@ -1,4 +1,33 @@
 pub fn vector_collection() {
-    let v: Vec<i32> = Vec::new();
+    let _v: Vec<i32> = Vec::new();
     let v = vec![1, 2, 3];
+    for val in v {
+        println!("{}", val);
+    }
+    let mut other_v = Vec::new();
+    other_v.push(5);
+    other_v.push(6);
+    other_v.push(7);
+    other_v.push(8);
+    for val in other_v {
+        println!("{}", val);
+    }
+
+    {
+        let mut v = Vec::new();
+        v.push(1);
+        v.push(2);
+        v.push(3);
+        let third: &i32 = &v[2];
+        println!("Third element: {}", third);
+    }
+
+    {
+        let v = vec![1, 2];
+        let third: Option<&i32> = v.get(2);
+        match third {
+            Some(third) => println!("Third element: {}", third),
+            None => println!("There's no third element"),
+        }
+    }
 }
